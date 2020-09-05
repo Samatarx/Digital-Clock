4159fd7dcf2c6fee69405b1db764c00a6f7f9e0d
Samatar Xasan <68086659+Samatarx@users.noreply.github.com>
2020-09-05 22:44:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-function tikTok() {
+function time() {
 
   var date = new Date();
 
   let hr = date.getHours();
-
   let min = date.getMinutes();
-
   let sec = date.getSeconds();
 
-  var x = 0
+
+  /**
+    @desc Calculates the Seconds so that it doesn't show a single number i.e. "0"
+    @param @x and time ()
+    @return the time in correct format 00:00:00
+  **/
+  
 
   function simpleCalc(x) {
-    return x < 10 ? x = "0" + x : x;
+     x < 10 ? x = "0" + x : x;
+     return x
   }
 
-  document.querySelector(".foota").innerHTML = simpleCalc(hr) + ":" + simpleCalc(min) + ":" + simpleCalc(sec);
+
+  /**
+    @desc Adds the variables together in the correct time format
+    @param  document.querySelector(".foota").innerHTML 
+    @return the time in correct format 00:00:00
+  **/
+  document.querySelector(".hours").innerHTML = ` <span>${simpleCalc(hr)}</span> : <span>${simpleCalc(min)}</span> : <span>${simpleCalc(sec)}</span>`;
 
 }
 
-setInterval = setInterval(tikTok, 1000);
+setInterval = setInterval(time, 1000);
